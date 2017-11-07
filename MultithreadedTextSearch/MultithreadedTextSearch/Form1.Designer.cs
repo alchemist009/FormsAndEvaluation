71bb5464c7e6efbbab6c7937f5eb02e04456d6ba
alchemist009 <alchemist009@gmail.com>
2017-11-06 18:31:03 -0600

    Added code for file browse

--- MultithreadedTextSearch/MultithreadedTextSearch/Form1.Designer.cs
+++ MultithreadedTextSearch/MultithreadedTextSearch/Form1.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.selectedFile = new System.Windows.Forms.TextBox();
+            this.searchWord = new System.Windows.Forms.TextBox();
             this.browseButton = new System.Windows.Forms.Button();
             this.occurrenceList = new System.Windows.Forms.ListBox();
             this.searchButton = new System.Windows.Forms.Button();
@@ -40,25 +40,27 @@
             this.totalTimeLabel = new System.Windows.Forms.Label();
             this.totalTimeBox = new System.Windows.Forms.TextBox();
             this.abortButton = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // textBox1
+            // selectedFile
             // 
-            this.textBox1.Location = new System.Drawing.Point(98, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(633, 20);
-            this.textBox1.TabIndex = 0;
+            this.selectedFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.selectedFile.Location = new System.Drawing.Point(83, 42);
+            this.selectedFile.Name = "selectedFile";
+            this.selectedFile.Size = new System.Drawing.Size(633, 20);
+            this.selectedFile.TabIndex = 0;
             // 
-            // textBox2
+            // searchWord
             // 
-            this.textBox2.Location = new System.Drawing.Point(98, 101);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(633, 20);
-            this.textBox2.TabIndex = 1;
+            this.searchWord.Location = new System.Drawing.Point(83, 98);
+            this.searchWord.Name = "searchWord";
+            this.searchWord.Size = new System.Drawing.Size(633, 20);
+            this.searchWord.TabIndex = 1;
             // 
             // browseButton
             // 
-            this.browseButton.Location = new System.Drawing.Point(769, 42);
+            this.browseButton.Location = new System.Drawing.Point(748, 42);
             this.browseButton.Name = "browseButton";
             this.browseButton.Size = new System.Drawing.Size(75, 23);
             this.browseButton.TabIndex = 2;
@@ -75,7 +77,7 @@
             // 
             // searchButton
             // 
-            this.searchButton.Location = new System.Drawing.Point(701, 201);
+            this.searchButton.Location = new System.Drawing.Point(694, 199);
             this.searchButton.Name = "searchButton";
             this.searchButton.Size = new System.Drawing.Size(156, 80);
             this.searchButton.TabIndex = 5;
@@ -85,16 +87,16 @@
             // fileName
             // 
             this.fileName.AutoSize = true;
-            this.fileName.Location = new System.Drawing.Point(30, 48);
+            this.fileName.Location = new System.Drawing.Point(12, 42);
             this.fileName.Name = "fileName";
-            this.fileName.Size = new System.Drawing.Size(54, 13);
+            this.fileName.Size = new System.Drawing.Size(63, 13);
             this.fileName.TabIndex = 6;
-            this.fileName.Text = "File Name";
+            this.fileName.Text = "File Name : ";
             // 
             // findWord
             // 
             this.findWord.AutoSize = true;
-            this.findWord.Location = new System.Drawing.Point(30, 104);
+            this.findWord.Location = new System.Drawing.Point(12, 101);
             this.findWord.Name = "findWord";
             this.findWord.Size = new System.Drawing.Size(65, 13);
             this.findWord.TabIndex = 7;
@@ -110,7 +112,7 @@
             // instanceLabel
             // 
             this.instanceLabel.AutoSize = true;
-            this.instanceLabel.Location = new System.Drawing.Point(660, 482);
+            this.instanceLabel.Location = new System.Drawing.Point(663, 475);
             this.instanceLabel.Name = "instanceLabel";
             this.instanceLabel.Size = new System.Drawing.Size(89, 13);
             this.instanceLabel.TabIndex = 9;
@@ -119,7 +121,7 @@
             // totalTimeLabel
             // 
             this.totalTimeLabel.AutoSize = true;
-            this.totalTimeLabel.Location = new System.Drawing.Point(660, 539);
+            this.totalTimeLabel.Location = new System.Drawing.Point(663, 526);
             this.totalTimeLabel.Name = "totalTimeLabel";
             this.totalTimeLabel.Size = new System.Drawing.Size(92, 13);
             this.totalTimeLabel.TabIndex = 10;
@@ -127,25 +129,30 @@
             // 
             // totalTimeBox
             // 
-            this.totalTimeBox.Location = new System.Drawing.Point(769, 539);
+            this.totalTimeBox.Location = new System.Drawing.Point(769, 526);
             this.totalTimeBox.Name = "totalTimeBox";
             this.totalTimeBox.Size = new System.Drawing.Size(100, 20);
             this.totalTimeBox.TabIndex = 11;
             // 
             // abortButton
             // 
-            this.abortButton.Location = new System.Drawing.Point(701, 333);
+            this.abortButton.Location = new System.Drawing.Point(694, 331);
             this.abortButton.Name = "abortButton";
             this.abortButton.Size = new System.Drawing.Size(156, 80);
             this.abortButton.TabIndex = 12;
-            this.abortButton.Text = "Abort";
+            this.abortButton.Text = "Abort!!";
             this.abortButton.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 606);
+            this.ClientSize = new System.Drawing.Size(878, 606);
             this.Controls.Add(this.abortButton);
             this.Controls.Add(this.totalTimeBox);
             this.Controls.Add(this.totalTimeLabel);
@@ -156,8 +163,8 @@
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.occurrenceList);
             this.Controls.Add(this.browseButton);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.searchWord);
+            this.Controls.Add(this.selectedFile);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -167,8 +174,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox selectedFile;
+        private System.Windows.Forms.TextBox searchWord;
         private System.Windows.Forms.Button browseButton;
         private System.Windows.Forms.ListBox occurrenceList;
         private System.Windows.Forms.Button searchButton;
@@ -179,6 +186,7 @@
         private System.Windows.Forms.Label totalTimeLabel;
         private System.Windows.Forms.TextBox totalTimeBox;
         private System.Windows.Forms.Button abortButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- MultithreadedTextSearch/MultithreadedTextSearch/Form1.cs
+++ MultithreadedTextSearch/MultithreadedTextSearch/Form1.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MultithreadedTextSearch
 {
     public partial class Form1 : Form
     {
+        string file;
+        string TEXT_FILE;
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog2.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                file = openFileDialog2.FileName;
+                try
+                {
+                    int lineCount = File.ReadAllLines(file).Count();
+                    selectedFile.Text = file;
+                }
+                catch(IOException)
+                {
+
+                }
+            }
+                
+        }
+
+
+        private void openFileDialog2_FileOk_1(object sender, CancelEventArgs e)
+        {
+
         }
     }
 }
