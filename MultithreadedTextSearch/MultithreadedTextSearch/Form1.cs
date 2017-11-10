85c3571a99f692db78c26db88eaba180f0cb1ee8
alchemist009 <alchemist009@gmail.com>
2017-11-09 18:55:20 -0600

    Added comments and code to handle form closing:
    * Form closing still doesn't stop BW
    * Added headers and comments for each method

--- MultithreadedTextSearch/MultithreadedTextSearch/Form1.cs
+++ MultithreadedTextSearch/MultithreadedTextSearch/Form1.cs
@@ -1,11 +1,18 @@
-﻿/// ========================================================
+﻿/// =========================================================================================================================================
 /// AUTHOR :        Gunjan Tomer
 /// CREATE DATE :   11/10/2017
-/// PURPOSE :       
+/// PURPOSE :       Multithreaded Text Search
+/// 
+/// DESCRIPTION :
+///This program takes as input a plain text file and performs a search for a specific word or string in it.
+///A browse button allows selecting a file from the disk and another box takes as input the string ti be searched in the file.
+///There two buttons in the layout's right side, one to begin search and another to clear the search results once the search concludes 
+///or the process is cancelled. Once the search button is clicked the backgroundworker starts searching the file line by line and
+///populates the listview with the results. The search button now changes to "Abort" and can be clicked at any time to cancel the search.
+///Once the search concludes a chime sound is played. The current status of the program is always displayed in the status bar at the 
+///bottom. A text box is also provided to keep a count of the matching results found till that point in time.
 ///
-///
-///
-///
+///===========================================================================================================================================
 ///
 using System;
 using System.Collections.Generic;
@@ -25,10 +32,10 @@ namespace MultithreadedTextSearch
     public partial class Form1 : Form
     {
         string file;
-        string TEXT_FILE;
         int lineCount;
         private System.ComponentModel.BackgroundWorker b_worker;
-        Boolean button_flag = false;
+        bool button_flag = false;
+        private bool closePending;
 
         public Form1()
         {
@@ -43,6 +50,11 @@ namespace MultithreadedTextSearch
             toolStripStatusLabel1.Text = "Ready for operation";
         }
 
+        /**
+         * Method to display an openFileDialog when the Browse button is clicked.
+         * Reads a file from the disk an displays its path in the selectedFile 
+         * text box.
+         */
         private void browseButton_Click(object sender, EventArgs e)
         {
             DialogResult result = openFileDialog2.ShowDialog();
@@ -62,34 +74,12 @@ namespace MultithreadedTextSearch
 
         }
 
-
-        void b_worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-
-            if(e.Cancelled)
-            {
-                toolStripStatusLabel1.Text = "Search cancelled";
-                statusStrip1.Invalidate();
-                statusStrip1.Refresh();
-            }
-
-            else if(e.Error != null)
-            {
-                toolStripStatusLabel1.Text = "Error while performing search";
-                statusStrip1.Invalidate();
-                statusStrip1.Refresh();
-            }
-            /*
-            else
-            {
-                toolStripStatusLabel1.Text = "Searching...";
-                statusStrip1.Invalidate();
-                statusStrip1.Refresh();
-            }
-            */
-        }
-
-
+        /**
+         * The main functionality of the backgroundworker. The file selected is stored in a 
+         * StreamReader object and read line by line to find a match all the while updating
+         * the listview and the instance count box.
+         * 
+         */ 
         async void b_worker_Scan_File(object sender, DoWorkEventArgs e)
         {
             List<String []> strList = new List<string []>();
@@ -114,13 +104,14 @@ namespace MultithreadedTextSearch
                         BeginInvoke
                         ((MethodInvoker)delegate
                             {
-                                listView1.Items.Add(lstitem);                                
+                                listView1.Items.Add(lstitem);
+                                instancesFoundBox.Text = listView1.Items.Count.ToString();
                             }
                         );
                     }
                 }
                 lineNumber++;
-               // await Task.Delay(1);
+                await Task.Delay(1);
             }
             BeginInvoke
                 ((MethodInvoker)delegate
@@ -128,7 +119,6 @@ namespace MultithreadedTextSearch
                         searchButton.Text = "Search";
                         button_flag = !(button_flag);
                         clearButton.Enabled = true;
-                        instancesFoundBox.Text = listView1.Items.Count.ToString();
                     }
                 );
             SoundPlayer simpleSound = new SoundPlayer(MultithreadedTextSearch.Properties.Resources.chimes);
@@ -136,34 +126,56 @@ namespace MultithreadedTextSearch
             toolStripStatusLabel1.Text = "Search finished";
         }
 
+
         private void b_worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
 
         }
 
 
+        /**
+         * Method to implement the Search/Abort functionality. Each consecutive press of the button 
+         * changes the mode of operation of the button.
+         * The status bar is used to display the current mode od operation.
+         */ 
         private void searchButton_Click(object sender, EventArgs e)
         {
-            if (button_flag == false)
+            if(file == null)
             {
-                listView1.Items.Clear();
-                b_worker.RunWorkerAsync();
-                searchButton.Text = "Abort";
-                button_flag = true;
-                toolStripStatusLabel1.Text = "Performing search...";
-                clearButton.Enabled = false;
+                MessageBox.Show("Please select a file before starting the search", "Error");
+                
             }
             else
             {
-                b_worker.CancelAsync();
-                button_flag = false;
-                searchButton.Text = "Search";
-                toolStripStatusLabel1.Text = "Ready for operation";
-                clearButton.Enabled = true;
-                instancesFoundBox.Text = (listView1.Items.Count).ToString();
+                if (button_flag == false)
+                {
+                    listView1.Items.Clear();
+                    b_worker.RunWorkerAsync();
+                    searchButton.Text = "Abort";
+                    button_flag = true;
+                    toolStripStatusLabel1.Text = "Performing search...";
+                    clearButton.Enabled = false;
+                }
+                else
+                {
+                    b_worker.CancelAsync();
+                    button_flag = false;
+                    searchButton.Text = "Search";
+                    toolStripStatusLabel1.Text = "Ready for operation";
+                    clearButton.Enabled = true;
+                    instancesFoundBox.Text = (listView1.Items.Count).ToString();
+                }
             }
+           
         }
 
+
+        /**
+         * Method to implement Clear button functionality. 
+         * Pressing the clear button either after search completion or after 
+         * having aborted the search clears the listview and the instance count box
+         * to get ready for a new search.
+         */
         private void clearButton_Click(object sender, EventArgs e)
         {
             listView1.Items.Clear();
@@ -175,5 +187,49 @@ namespace MultithreadedTextSearch
         {
 
         }
+
+        /*
+         *Method which runs once the backgroundworker finishes operations or  
+         * is interrupted before search completion. 
+         */
+        void b_worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (closePending) this.Close();
+            closePending = false;
+
+            if (e.Cancelled)
+            {
+                toolStripStatusLabel1.Text = "Search cancelled";
+                statusStrip1.Invalidate();
+                statusStrip1.Refresh();
+            }
+
+            else if (e.Error != null)
+            {
+                toolStripStatusLabel1.Text = "Error while performing search";
+                statusStrip1.Invalidate();
+                statusStrip1.Refresh();
+            }
+        }
+
+        /**
+         * Method to handle the form closing event. If the form is closed in the midst of
+         * a search operation, the backgroundworker is cancelled before closing the form.
+         * 
+         */
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+
+            if (b_worker.IsBusy)
+            {
+                closePending = true;
+                b_worker.CancelAsync();
+                e.Cancel = true;
+                this.Enabled = false;
+                return;
+            }
+            base.OnFormClosing(e);
+        }
+
     }
 }
